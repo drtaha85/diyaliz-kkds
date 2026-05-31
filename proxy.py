95673a219554fd030297be1cb5d014beb8fe0236
drtaha85 <drtaha85@hotmail.com>
2026-05-31 16:28:11 +0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -72,4 +72,5 @@ if __name__ == '__main__':
     t = threading.Thread(target=tarayici_ac)
     t.daemon = True
     t.start()
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
